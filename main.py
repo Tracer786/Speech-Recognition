cfc290a6520e11c13268cae9f67dbcad96c02a1d
Tracer786 <msingh6_be20@thapar.edu>
2024-03-04 18:25:15 +0530

    Upload file
    A code we get from the Assembly AI website for uploading the file. We can upload the file by directly specifying the name in the code or providing the name of the file during the run time.

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import requests
+# requests library is required to talk to the Assembly AI's API.
+
+from api_secrets import API_KEY_ASSEMBLYAI
+
+
+def read_file(filename, chunk_size=5242880):
+    with open(filename, 'rb') as _file:
+        while True:
+            data = _file.read(chunk_size)
+            if not data:
+                break
+            yield data
+            
+            
+headers = {'authorization': API_KEY_ASSEMBLYAI} 
+response = requests.post('https://api.assemblyai.com/v2/upload',
+                             headers=headers,
+                             data=read_file(filename))
+
 
 # upload
 
