985386b87e03c866d611f388576c4166b8a61304
Tracer786 <msingh6_be20@thapar.edu>
2024-03-04 18:29:49 +0530

    Update main.py
    Updated the code to get the file name from the terminal

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import requests
 
 from api_secrets import API_KEY_ASSEMBLYAI
 
+import sys  # to get the file name from the terminal
+
+upload_endpoint = "'https://api.assemblyai.com/v2/upload'"
+
+filename = sys.argv[1]
+
 
 def read_file(filename, chunk_size=5242880):
     with open(filename, 'rb') as _file:
@@ -14,9 +20,14 @@ def read_file(filename, chunk_size=5242880):
             
             
 headers = {'authorization': API_KEY_ASSEMBLYAI} 
-response = requests.post('https://api.assemblyai.com/v2/upload',
+response = requests.post(upload_endpoint,
                              headers=headers,
                              data=read_file(filename))
+
+# we are doing a post request while uploading the file to Assembly AI.
+
+
+print(response.json())  # after uploading the file we need to check what kind of response we are getting
 
 
 # upload
