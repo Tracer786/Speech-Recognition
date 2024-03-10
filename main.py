ce878ea6bf06301792fe4ba3d44a5f01e2956e29
Tracer786 <msingh6_be20@thapar.edu>
2024-03-10 10:34:42 +0530

    Completed
    We have fully organized the code. Now we can easily understand and run the code.

--- main.py
+++ main.py
@@ -1,56 +1,7 @@
-import requests   # requests library is required to talk to the Assembly AI's API.
-from api_secrets import API_KEY_ASSEMBLYAI
 import sys  # to get the file name from the terminal
-import time
+from api_communication import * 
 
-upload_endpoint = 'https://api.assemblyai.com/v2/upload'
-transcript_endpoint = 'https://api.assemblyai.com/v2/transcript'
-headers = {'authorization': API_KEY_ASSEMBLYAI} 
 filename = sys.argv[1]
 
-
-
-# transcribe
-def transcribe(audio_url):
-    transcript_request = { "audio_url": audio_url}
-    transcript_response = requests.post(transcript_endpoint, json=transcript_request, headers=headers)
-    # print(response.json())
-    job_id = transcript_response.json()['id']
-    return job_id
-
-# polling 
-def poll(transcript_id):
-    polling_endpoint = transcript_endpoint + '/' + transcript_id
-    polling_response = requests.get(polling_endpoint, headers = headers)
-    # print(polling_response)
-    # print(polling_response.json())
-    return polling_response.json()
-
-#will check whether the polling is complete or not
-def get_transcription_result_url(audio_url):
-    transcript_id = transcribe(audio_url)
-    while True:
-        data = poll(transcript_id)
-        if data['status'] == 'completed':
-            return data, None
-        elif data['status'] == 'error':
-            return data, data['error']
-        
-        print('Waiting 30 seconds...')
-        time.sleep(30)
-
-# save transcription
-def save_transcript(audio_url):
-    data,error = get_transcription_result_url(audio_url)
-
-    if data:
-        text_filename = filename + ".txt"
-        with open(text_filename, "w") as f:
-            f.write(data['text'])
-        print('Transcription saved!!')
-    elif error:
-        print("Error!!", error)
-    # print(data)
-
 audio_url = upload(filename)
-save_transcript(audio_url)
+save_transcript(audio_url,filename)
